d8ea32a847ed9207f9b9a514a52e16ff6aa3cd20
marcuslarsson92 <marcuslarsson92@gmail.com>
2023-04-06 01:32:21 +0200

    La till metod sendPostRequest()
    La till ett "onclick" på contact button som startar metoden sendPostRequest() och den metoden skickar iväg namn, email, dob till spring boot servern som sedan lagrar i databasen

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ function increment() {
     count++
     countEl.innerHTML = count;
 }
+
+
+function sendPostRequest() {
+    const url = 'http://localhost:8080/api/v1/member';
+    const data = {
+      name: 'Simon',
+      email: 'simonflenman1@gmail.com',
+      dob: '2000-03-26'
+    };
+  
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+  }
+  
