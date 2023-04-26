b0921d989ff97bf37e971052723ef8ad89c625e8
simonflenman <simon.flenman1@gmail.com>
2023-04-26 14:08:24 +0200

    Implementerat Registrerings samt inloggnings funktioner i javascript
    Dessa funktioner ska skicka en request till servern för att lägga till en användare i databasen eller jämföra inputen från användaren med vad som finns i databasen för att kunna logga in

--- loginPage.js
+++ loginPage.js
@@ -2,10 +2,27 @@
 let email;
 let password;
 
-document.getElementById("loginButton").onclick = function(){
-    email = document.getElementById("email").value;
-    password = document.getElementById("passw").value;
-
-    console.log(email);
-    console.log(password);
+function login(){
+        email = document.getElementById("email").value;
+        password = document.getElementById("passw").value;
+        console.log(email);
+        console.log(password);
+        
+    
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === XMLHttpRequest.DONE){
+                if(xhr.status === 200){
+                    window.location.href = "alt/altHomePage";
+                }else if(xhr.status === 401){
+                    alert("Wrong E-Main or password");
+                }
+            }
+        };
+        
+    
+        xhr.open('POST', '/...');
+        xhr.setRequestHeader('Contetnt-Type', 'application/json');
+        xhr.send(JSON.stringify({email, password}));
 }
+        
--- signUp.js
+++ signUp.js
@@ -3,12 +3,31 @@ let lastName;
 let email;
 let password;
 
-document.getElementById("signUpButton").onclick = function(){
+function sendPostRequest() {
     firstName = document.getElementById("fname").value;
     lastName = document.getElementById("lname").value;   
     email = document.getElementById("email").value;
     password = document.getElementById("passw").value;
 
+    const url = 'http://localhost:8080/api/v1/member';
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    };
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+    
     console.log(firstName);
     console.log(lastName);
     console.log(email);
