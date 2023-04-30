85a535aacb0b2c95b6ebab2d3b47d29b8978c904
Aussdinpappa <127444830+Aussdinpappa@users.noreply.github.com>
2023-04-30 12:10:12 +0200

    jag har skapat en ny style element (gradient) och lade till en linjär gradient bakgrund.
    Animerad rubrik, satt textfärger till tranperant och lade till en linjär
    gradientbakgrund för rubriken.
    
    ändrade färgerna och bakgrundsfärger för knappar
    
    Ändrade sidmarginaler och lade till en inneslutande kantlinje för sidan.

--- index.js
+++ index.js
@@ -42,3 +42,47 @@ function sendPostRequest() {
   });
 
   }
+
+  const gradient = document.createElement('style');
+gradient.innerHTML = `
+  body {
+    background: linear-gradient(135deg, #84FAB0 10%, #8FD3F4 100%);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+  }
+
+  @keyframes gradient {
+    0% { background-position: 0% 50%; }
+    50% { background-position: 100% 50%; }
+    100% { background-position: 0% 50%; }
+  }
+`;
+document.head.appendChild(gradient);
+
+const heading = document.querySelector('h1');
+if (heading) {
+  heading.style.color = 'transparent';
+  heading.style.backgroundImage = 'linear-gradient(45deg, #84FAB0, #8FD3F4)';
+  heading.style.backgroundSize = '200% auto';
+  heading.style.webkitBackgroundClip = 'text';
+  heading.style.animation = 'shine 5s linear infinite';
+}
+
+const shine = document.createElement('style');
+shine.innerHTML = `
+  @keyframes shine {
+    0% { background-position: 200% center; }
+    100% { background-position: -200% center; }
+  }
+`;
+document.head.appendChild(shine);
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.style.backgroundColor = '#4CAF50';
+    button.style.color = '#ffffff';
+});
+
+document.body.style.margin = '20px';
+document.body.style.border = '2px solid #ccc';
+
