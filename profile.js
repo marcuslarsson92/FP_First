5e2655d135c89eeb13cae7004236ad74ea229413
alexandraaholter <alexandraaholter@gmail.com>
2023-08-10 15:42:38 +0200

    updatera till Delete

--- profile.js
+++ profile.js
@@ -277,11 +277,11 @@ function deleteUser(userID) {
   const url = "http://localhost:8080/api/v1/member";
 
   fetch(url, {
-    method: "POST",
+    method: "DELETE",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ email})
+    body: JSON.stringify({ userID})
   })
     .then(response => {
       if (response.ok) {
